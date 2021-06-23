ef1eafbfbe75e0f6771a66f92f53f578d20c2088
samuelghill <samuelgarretthill@gmail.com>
2021-06-23 15:43:37 -0500

    first commit

---
+++ pages/product/[slug].tsx
@@ -0,0 +1,50 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import { Layout } from '@components/common'
+import { getAllProductsWithSlug, getProductBySlug } from '../../lib/api'
+import { ProductView } from '@components/product'
+
+export default function Slug(product: any) {
+  const router = useRouter()
+
+  console.log('router', router.isFallback)
+  console.log('product is here in slug', product)
+  // console.log('product', product.product.productByAgilityId)
+
+  if (!router.isFallback && !product.product.productByAgilitySlug) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+    <>
+      {router.isFallback ? (
+        <h1>Loading…</h1>
+      ) : (
+        <>
+          <ProductView product={product?.product.productByAgilitySlug} />
+        </>
+      )}
+    </>
+  )
+}
+
+export async function getServerSideProps(params: any) {
+  console.log('params', params.params)
+  const data = await getProductBySlug(params.params.slug)
+  // console.log('staticprops', data)
+  return {
+    props: {
+      product: data,
+    },
+  }
+}
+
+export async function getServerSidePaths() {
+  const products = await getAllProductsWithSlug()
+  // console.log('staticpaths', products)
+  return {
+    paths: products.map((node: any) => `/en-US/product/${node.slug}`),
+    fallback: false,
+  }
+}
+
+Slug.Layout = Layout
